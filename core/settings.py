e2915316fa1c584ae69db7393406c293720f5f10
jona44 <55057208+jona44@users.noreply.github.com>
2026-06-27 12:34:41 +0200

    06/27/2026-02

--- core/settings.py
+++ core/settings.py
@@ -255,7 +255,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
